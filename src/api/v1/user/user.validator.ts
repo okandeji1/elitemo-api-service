e1ebefdc38ce35bbfdb580762eab1daa259db057
okandeji <okandeji2012@gmail.com>
2022-09-06 20:15:27 +0100

    fix: migration validation

--- src/api/v1/user/user.validator.ts
+++ src/api/v1/user/user.validator.ts
@@ -4,7 +4,6 @@ export const setupTenantSchema = joi.object({
   name: joi.string().required(),
   company: joi.string().required(),
   website: joi.string(),
-  expiresAt: joi.date(),
   status: joi.any().valid('PENDING', 'ACTIVE', 'INACTIVE'),
 });
 
